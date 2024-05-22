9f298000d073d93d869344fc2f14c98940c471e6
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-22 23:57:30 +0100

    programme qui demande à l’utilisateur de saisir deux nombres a et b et de lui afficher leur maximum

---
+++ exercice_3.py
@@ -0,0 +1,5 @@
+premier_nombre  = input("Veuillez saisir le premier nombre a : ")
+deuxieme_nombre = input("Veuillez saisir le deuxième nombre b : ")
+
+maximum = max(premier_nombre , deuxieme_nombre)
+print("Le maximum entre", premier_nombre , "et", deuxieme_nombre, "est", maximum)
