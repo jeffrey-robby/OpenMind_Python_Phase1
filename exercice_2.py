d81f3f4cd150ceff7865bb7d11b6369f1c54e132
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-22 23:14:39 +0100

    programme qui demande à l’utilisateur de saisir deux nombres a et b et de lui afficher leur somme

---
+++ exercice_2.py
@@ -0,0 +1,5 @@
+a = float(input("Veuillez saisir le premier nombre a : "))
+b = float(input("Veuillez saisir le deuxième nombre b : "))
+
+somme = a + b
+print("La somme de", a, "+", b, "est :", somme)
