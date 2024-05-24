c50d31938144ca5d739c8de0fd31bfb0626f1250
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-25 00:08:18 +0100

    programme qui demande à l’utilisateur de saisir un nombre entier et lui affiche si ce nombre est pair ou impair

---
+++ exercice_5.py
@@ -0,0 +1,6 @@
+nombre_entier = int(input("Veuillez saisir un nombre entier : "))
+
+if nombre_entier % 2 == 0:
+    print(nombre_entier, "est pair.")
+else:
+    print(nombre_entier, "est impair.")
