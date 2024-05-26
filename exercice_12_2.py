a98286ce47ef3e84e90b5eb9073932eb8128a93c
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-26 23:51:48 +0100

    programme qui affiche les tables de multiplications de tous les nombres compris entre 1 et 9

---
+++ exercice_12_2.py
@@ -0,0 +1,7 @@
+for i in range(1, 10):
+   print("Table de multiplication de", i, ":")
+   print()  # Ligne vide 
+   for j in range(1, 11):
+        resultat = i * j
+        print(i, "x", j, "=", resultat)
+        print()  # Ligne vide 
