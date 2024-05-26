61b2f6558d7dde616f6b856f748fb17bd6adb2f2
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-26 23:41:16 +0100

    programme qui demande à l’utilisateur de saisir le rayon d’un cercle et renvoi  sa surface et le périmètre.

---
+++ exercice_10.py
@@ -0,0 +1,8 @@
+rayon = float(input("Veuillez saisir le rayon du cercle : "))
+
+surface = 3.14 * rayon**2
+
+perimetre = 2 * 3.14 * rayon
+
+print("La surface est de :", surface)
+print("Le périmètre est de :", perimetre)
