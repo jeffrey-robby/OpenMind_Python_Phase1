95c2efcbc18e9734a96968ca144e563b5ed6811b
jeffrey <167925558+jeffrey-robby@users.noreply.github.com>
2024-05-26 23:53:45 +0100

    programme qui demande à l’utilisateur de saisir deux nombres entiers a et b et affiche le quotient et le reste de la division euclidienne de a par b

---
+++ exercice_13.py
@@ -0,0 +1,8 @@
+premier_nombre = int(input("Veuillez saisir le premier nombre entier a : "))
+deuxieme_nombre = int(input("Veuillez saisir le deuxième nombre entier b : "))
+
+quotient = premier_nombre / deuxieme_nombre
+reste = premier_nombre % deuxieme_nombre
+
+print("Quotient :", quotient)
+print("Reste :", reste)
